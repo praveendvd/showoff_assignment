0a3d12e24ce04e67d3b43fd1c0a1b91d4473f47d
Praveen <praveendvd@live.com>
2020-01-30 22:37:44 +0000

    sample_report

--- start.bat
+++ start.bat
@@ -1,1 +1,4 @@
 .\node_modules\.bin\newman run "Rails React App.postman_collection.json" -e "Rails React App Production.postman_environment.json" --folder Data_Driven_User_Create -d data.csv  -r htmlextra,cli --reporter-htmlextra-logs
+
+
+.\node_modules\.bin\newman run "Rails React App.postman_collection.json" -e "Rails React App Production.postman_environment.json" --folder Data_Driven_User_Create -d data.csv  -r htmlextra,cli --reporter-htmlextra-logs
